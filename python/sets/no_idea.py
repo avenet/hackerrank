8a10cee73770faad9b890eb8fc074e0c7616183f
Andy Venet <andy.venet@gmail.com>
2017-09-02 00:01:59 -0300

    Improved implementation for No idea challenge

--- python/sets/no_idea.py
+++ python/sets/no_idea.py
@@ -1,18 +1,16 @@
 n, m = map(int, input().split())
-
 items = list(map(int, input().split()))
-set_a = list(map(int, input().split()))
-set_b = list(map(int, input().split()))
+happiness_dict = {}
+
+for item in map(int, input().split()):
+    happiness_dict[item] = 1
+
+for item in map(int, input().split()):
+    happiness_dict[item] = -1
 
 happiness_level = 0
 
 for item in items:
-    in_a = item in set_a
-    in_b = item in set_b
-
-    if in_a and not in_b:
-        happiness_level += 1
-    elif in_b and not in_a:
-        happiness_level -= 1
+    happiness_level += happiness_dict.get(item, 0)
 
 print(happiness_level)
