1c39738a4242d2bbb2b00820378f61e484bd20a7
Andy Venet <andy.venet@gmail.com>
2017-10-04 22:11:32 -0300

    Solved HTML parser part one challenge

---
+++ python/regex_and_parsing/html_parser_part_one.py
@@ -0,0 +1,36 @@
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start : {}".format(tag))
+        self.print_attrs(attrs)
+    
+    def handle_endtag(self, tag):
+        print("End   : {}".format(tag))
+    
+    def handle_startendtag(self, tag, attrs):
+        print("Empty : {}".format(tag))
+        self.print_attrs(attrs)
+    
+    def print_attrs(self, attrs):
+        for attr_name, attr_value in attrs:
+            print(
+                '-> {0} > {1}'.format(
+                    attr_name,
+                    attr_value
+                )
+            )
+
+
+parser = MyHTMLParser()
+
+line_count = int(input())
+lines = []
+
+for line_read_attempt in range(line_count):
+    lines.append(input())
+
+html_content = '\n'.join(lines)
+
+parser.feed(html_content)
