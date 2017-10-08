351ddab8b6a760791edbcf018bac8a322e17a4b8
Andy Venet <andy.venet@gmail.com>
2017-10-08 09:34:58 -0300

    Improved regular expression to match alternating digits

--- python/regex_and_parsing/validating_postal_codes.py
+++ python/regex_and_parsing/validating_postal_codes.py
@@ -3,16 +3,7 @@ import re
 POSTAL_CODE_RE = re.compile(r'^[1-9]\d{5}$')
 
 ALTERNATING_DIGITS_RE = re.compile(
-    r'0(?=\d0)|'
-    r'1(?=\d1)|'
-    r'2(?=\d2)|'
-    r'3(?=\d3)|'
-    r'4(?=\d4)|'
-    r'5(?=\d5)|'
-    r'6(?=\d6)|'
-    r'7(?=\d7)|'
-    r'8(?=\d8)|'
-    r'9(?=\d9)'
+    r'(\d)(?=\d\1)'
 )
 
 input_str = input()
