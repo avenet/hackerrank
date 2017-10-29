fa4e2c7adb4f4bb18659b34547c9eabf677a1859
Andy Venet <andy.venet@gmail.com>
2017-10-29 11:40:36 -0300

    Fixed some messy spaces on Climbing the leaderboard challenge solution

--- algorithms/implementation/climbing_the_leaderboard.py
+++ algorithms/implementation/climbing_the_leaderboard.py
@@ -3,51 +3,51 @@ import sys
 
 def get_leaderboard(player_scores):
     result = []
-    
+
     leaderboard_scores = sorted(
         player_scores,
         reverse=True
     )
-    
+
     previous_score = sys.maxsize
-    
+
     for leaderboard_score in leaderboard_scores:
         if previous_score != leaderboard_score:
             result.append(leaderboard_score)
-        
+
         previous_score = leaderboard_score
-    
+
     return result
 
 
 def binary_search(leaderboard_values, score):
     i = 0
     j = len(leaderboard_values) - 1
-    
+
     if score <= leaderboard_values[-1]:
         return j
     elif score == leaderboard_values[0]:
         return 0
     elif score > leaderboard_values[0]:
         return -1
-    
+
     while i < j:
         mid = (j + i) // 2
         middle_element = leaderboard_values[mid]
-        
+
         if mid == i:
             return i
-        
+
         if score > middle_element:
             j = mid
         elif score < middle_element:
             i = mid
         else:
             return mid
-    
+
     if leaderboard_values[j] > score:
         return j
-    
+
     return i
 
 
@@ -56,12 +56,12 @@ def get_leaderboard_position(current_leaderboard, target_score):
         current_leaderboard,
         target_score
     )
-    
+
     result = 1 + position
-    
+
     if current_leaderboard[position] == target_score:
         return result
-    
+
     return 1 + result
 
 
