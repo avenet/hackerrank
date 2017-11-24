84d39373c2b87bd94dbcc9a910263c1b34906d9e
Andy Venet <andy.venet@gmail.com>
2017-11-24 23:58:51 +0000

    Added already solved Super reduced string challenge

---
+++ algorithms/strings/super_reduced_string.py
@@ -0,0 +1,21 @@
+def super_reduced_string(str_value):
+    result = str_value
+
+    while True:
+        previous_length = len(result)
+        unique_letters = set(result)
+
+        for unique_letter in unique_letters:
+            result = result.replace(unique_letter * 2, '')
+
+        if len(result) == previous_length:
+            break
+
+    if not result:
+        return 'Empty String'
+
+    return result
+
+s = input().strip()
+
+print(super_reduced_string(s))
